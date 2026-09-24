e84c5fc4c4d2ce7c73a7b3684d65c95ccd9086ee
kshitijdev24 <kshitij.chetan030174@gmail.com>
2026-09-24 19:45:16 +0530

    day-1 updated

--- DSA-basic/day-1/app.js
+++ DSA-basic/day-1/app.js
@@ -1,10 +1,10 @@
 
-console.log("pattern-1")
+console.log("pattern-1a")
 let n = 3
-for(let rows=1;rows<=n;rows++){
-    let str="";
-    for (let cols = 1; cols <= n; cols++){
-        str+="* "
+for (let rows = 1; rows <= n; rows++) {
+    let str = "";
+    for (let cols = 1; cols <= n; cols++) {
+        str += "* "
     }
     console.log(str)
 }
@@ -14,13 +14,13 @@ let m = 3
 for (let rows = 1; rows <= m; rows++) {
     let str = "";
     for (let cols = 1; cols <= m; cols++) {
-        str +=  rows+" "
+        str += rows + " "
     }
     console.log(str)
 }
 
 console.log("pattern-3")
-let k= 3
+let k = 3
 for (let rows = 1; rows <= k; rows++) {
     let str = "";
     for (let cols = 1; cols <= k; cols++) {
@@ -31,21 +31,21 @@ for (let rows = 1; rows <= k; rows++) {
 
 console.log("pattern-4")
 let w = 4
-for (let rows = 1; rows <= w; rows++){
+for (let rows = 1; rows <= w; rows++) {
     let str = ""
-    for (let cols = 1; cols <= w; cols++){
+    for (let cols = 1; cols <= w; cols++) {
         if (rows == 1 || rows == w) {
-            str+="* "
+            str += "* "
         }
         else {
-            if (cols == 1 ) {
-                str+="* "
+            if (cols == 1) {
+                str += "* "
             }
-            if (cols != w || cols!=1) {
-                str+=" "
+            if (cols != w || cols != 1) {
+                str += " "
             }
-            if(cols==w){
-                str+="* "
+            if (cols == w) {
+                str += "* "
             }
         }
     }
@@ -54,9 +54,9 @@ for (let rows = 1; rows <= w; rows++){
 
 console.log("pattern-5")
 let a = 4;
-for (let rows = 1; rows <= a; rows++){
+for (let rows = 1; rows <= a; rows++) {
     let str = ""
-    for (let cols = 1; cols <= a; cols++){
+    for (let cols = 1; cols <= a; cols++) {
         str += String.fromCharCode(64 + rows)
     }
     console.log(str)
@@ -73,23 +73,24 @@ for (let rows = 1; rows <= b; rows++) {
 }
 
 console.log("pattern-7")
-let counter = 0;
 let z = 4;
-for (let i = 1; i <= z; i++){
-    let str =""
-    for (let j = 1; j <= z; j++){
+for (let i = 1; i <= z; i++) {
+    let str = ""
+    for (let j = 1; j <= z; j++) {
         if (j == 1 || j == z || i == 1 || i == z) {
-            
-            str += i + counter + " "
-            counter++
+
+            str += i  + " "
+        
         }
         else {
-           
+
             str += "  "
-            counter++
+        
+
         }
+    
     }
-    counter--
+
     console.log(str)
 }
 
@@ -101,7 +102,7 @@ for (let i = 1; i <= x; i++) {
     for (let j = 1; j <= x; j++) {
         if (j == 1 || j == x || i == 1 || i == x) {
 
-            str +=  String.fromCharCode(count+i) + " "
+            str += String.fromCharCode(count + i) + " "
             count++
         }
         else {
@@ -116,13 +117,13 @@ for (let i = 1; i <= x; i++) {
 
 console.log("pattern-9")
 let counter1 = 1;
-let d= 4;
+let d = 4;
 for (let i = 1; i <= d; i++) {
     let str = ""
     for (let j = 1; j <= d; j++) {
         if (j == 1 || j == d || i == 1 || i == d) {
 
-            str +=counter1 + " "
+            str += counter1 + " "
             counter1++
         }
         else {
@@ -131,7 +132,7 @@ for (let i = 1; i <= d; i++) {
             counter1++
         }
     }
-   
+
     console.log(str)
 }
 
@@ -151,6 +152,58 @@ for (let i = 1; i <= f; i++) {
 
             str += "  "
             count2++
+        }
+    }
+    console.log(str)
+}
+
+
+console.log("pattern-11")
+let p= 5;
+for (let i = 1; i <= p; i++){
+    let str=""
+    for (let j = 1; j <= p; j++){
+        if (i == j) {
+            str+="* "
+        }
+        else {
+            str+=" "
+        }
+    }
+    console.log(str)
+}
+
+
+console.log("pattern-12")
+let q=5
+for (let i = 0; i < q; i++){
+    let str = ""
+    for (let j = 0; j < q; j++){
+        if (i + j == q-1 ) {
+            str+="* "
+        }
+        else {
+            str+=" "
+        }
+    }
+    console.log(str)
+}
+
+
+console.log("pattern-13")
+let r = 5
+for (let i = 1; i <= r; i++){
+    let str = ""
+    for (let j = 1; j <= r; j++){
+        if (i == 1 || i == r || j==1 || j==r) {
+            str+="* "
+        }
+        else if (i == j || i + j == r + 1) {
+            str+="* "
+        }
+     
+        else {
+            str+="  "
         }
     }
     console.log(str)
